2d7bfddaf4e4fb9320fc874a04e84802dc84c837
John <johndoe@gmail.com>
2019-03-31 16:48:39 -0400

    initial version

---
+++ BankingApp/src/main/java/com/mathcomputers/banking/BankAccount.java
@@ -0,0 +1,43 @@
+package com.mathcomputers.banking;
+
+public class BankAccount
+{
+    // Attributes
+    private double balance;
+
+    // Constructors
+    public BankAccount(double initialBalance)
+    {
+	this.balance = initialBalance;
+    }
+
+    // Methods
+    public void deposit(double amount)
+    {
+	this.balance = this.balance + amount;
+    }
+
+    public void withdraw(double amount)
+    {
+	if (amount <= this.balance)
+	    {
+		this.balance = this.balance - amount;
+	    }
+	else
+	    {
+		System.out.println("Sorry, insufficient funds.");
+		System.out.println(String.format("You are trying to withdraw $%.2f", amount));
+		System.out.println(String.format("but your account only contains $%.2f", this.balance));
+	    }
+    }
+
+    public double getBalance()
+    {
+	return this.balance;
+    }
+    
+    public String toString()
+    {
+	return String.format("After transactions, balance = $%.2f", this.balance);
+    }
+}
---
+++ BankingApp/src/main/java/com/mathcomputers/banking/BankClient.java
@@ -0,0 +1,13 @@
+package com.mathcomputers.banking;
+
+public class BankClient
+{
+    public static void main(String[] args)
+    {
+	BankAccount ba = new BankAccount(100);
+	System.out.println(String.format("Before transactions, balance = $%.2f", ba.getBalance()));
+	ba.deposit(76.35);
+	ba.withdraw(200.00);
+	System.out.println(ba.toString());
+    }
+}
