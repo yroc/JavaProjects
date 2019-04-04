f2c352624f0f17cfa5cc74a9e4337e4d67056ecc
John <johndoe@gmail.com>
2019-04-03 20:03:21 -0400

    Initialize

---
+++ FibonacciSeries/src/main/java/com/mathcomputers/math/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package com.mathcomputers.math;
+
+public class FibonacciSeries
+{
+    public static void main(String[] arguments)
+    {
+	int i_2 = 1;		// The (i - 2)th term
+	int i_1 = 1;		// The (i - 1)th term
+	int i;			// The ith term
+
+	// Print out the initial two terms
+	System.out.print(i_2 + ", ");
+	System.out.print(i_1 + ", ");
+
+	// Compute and print the next 17 terms
+	for (int n = 1; n <=17; n++)
+	    {
+		i = i_2 + i_1;
+		System.out.print(i + ", ");
+		i_2 = i_1;
+		i_1 = i;
+	    }
+
+	// Print out the last term
+	System.out.println(i_2 + i_1);
+    }
+}
