4391f3d4fcd3f227d949dd25388bf382d3bbbc6a
John <johndoe@gmail.com>
2019-04-12 21:56:43 -0400

    Initialize

---
+++ src/main/java/com/mathcomputers/StringManipulator.java
@@ -0,0 +1,17 @@
+package com.mathcomputers;
+
+import java.lang.*;
+
+/** A class that encapsulates a collection of string manipulation methods */
+
+public class StringManipulator
+{
+    // Methods
+    /** Given a string, returns a new string where the first and last characters
+     * have been exchanged */
+    public static String frontBack(String str)
+    {
+	if (str.length <= 1)
+	    {
+		return str;
+	    }
