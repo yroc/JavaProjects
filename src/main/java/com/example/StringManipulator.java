e56b045c2202bd3252809b9acfef91b7586190c8
John <johndoe@gmail.com>
2019-04-26 21:02:10 -0400

    Complete frontBack method of StringManipulator

--- src/main/java/com/example/StringManipulator.java
+++ src/main/java/com/example/StringManipulator.java
@@ -6,12 +6,45 @@ import java.lang.String;
 
 public class StringManipulator
 {
+    // Attributes
+    private String str;
+
+    // Constructor
+    public StringManipulator(String str)
+    {
+	this.str = str;
+    }
+
     // Methods
     /** Given a string, returns a new string where the first and last characters
      * have been exchanged */
-    public static String frontBack(String str)
+    public String frontBack()
     {
-	if (str.length <= 1)
+	if (this.str.length() <= 1)
 	    {
-		return str;
+		return this.str;
 	    }
+
+	// this.str.length() >= 2
+	char firstChar = this.str.charAt(0);
+	char lastChar = this.str.charAt(this.str.length() - 1);
+	String middleChars = this.str.substring(1, this.str.length() - 1);
+	return lastChar + middleChars + firstChar;
+    }
+
+    // PSVM test method
+    public static void main(String[] arguments)
+    {
+	StringManipulator smEmpty = new StringManipulator("");
+	StringManipulator smOne = new StringManipulator("a");
+	StringManipulator smTwo = new StringManipulator("ab");
+	StringManipulator smThree = new StringManipulator("abc");
+	StringManipulator smFive = new StringManipulator("Float");
+
+	System.out.println(smEmpty.frontBack() + ", ");
+	System.out.println(smOne.frontBack() + ", ");
+	System.out.println(smTwo.frontBack() + ", ");
+	System.out.println(smThree.frontBack() + ", ");
+	System.out.println(smFive.frontBack());
+    }
+}
