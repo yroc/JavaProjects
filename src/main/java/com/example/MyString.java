b4548c08e3d80d6a41dda29965559ab0c5b39cc6
John <johndoe@gmail.com>
2019-04-28 23:10:51 -0400

    Complete initial version

--- src/main/java/com/example/MyString.java
+++ src/main/java/com/example/MyString.java
@@ -52,11 +52,68 @@ public final class MyString
      * character sequence. Note that the use of this constructor is unnecessary
      * since Strings are immutable.
      */
+    public MyString()
+    {
+	this.value = new char[0];
+    }
 
-  public MyString() { this.value = new char[0]; }
-
+    /**
+     * Allocates a new {@code String} so that it represents the sequence of
+     * characters currently contained in the character array argument. The
+     * contents of the character array are copied; subsequent modification of
+     * the character array does not affect the newly-created string.
+     *
+     * @param value
+     *        The initial value of the string
+     */
     public MyString(char[] value)
     {
 	this.value = Arrays.copyOf(value, value.length);
     }
-}
+
+    // Methods
+    /**
+     * Returns the length of this string. The length is equal to the number of
+     * <a href="Character.html#unicode">Unicode code units</a> in the string.
+     *
+     * @return the length of the sequence of characters represented by this object.
+     */
+    public int length()
+    {
+	return this.value.length;
+    }
+
+    /**
+     * Returns {@code true} if, and only if, {@link #length()} is {@code 0}.
+     *
+     * @return {@code true} if {@link #length()} is {@code 0}, otherwise {@code
+     * false}
+     *
+     * @since 1.6
+     */
+    public boolean isEmpty()
+    {
+	return this.value.length == 0;
+    }
+
+    /**
+     * Returns {@code char} value at the specified index. An index ranges from
+     * {@code 0} to {@code length() - 1}. The first {@code char} value of the
+     * sequence is at index {@code 0}, the next at index {@code 1}, and so on,
+     * as for array indexing.
+     *
+     * @param      index  the index of the {@code char} value.
+     * @return     the {@code char} value at the specified index of this string.
+     *             The first {@code char} value is at index {@code 0}.
+     * @exception  IndexOutOfBoundsException if the {@code index} argument is
+     *             negative or not less than the length of this string.
+     */
+    public char charAt(int index)
+    {
+	if ((index < 0 || index >= this.value.length))
+	    {
+		throw new IndexOutOfBoundsException(index);
+	    }
+	return this.value[index];
+    }
+ }
